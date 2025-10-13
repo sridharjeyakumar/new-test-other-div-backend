3cbc2b7344c83fc068696343a29309078c648b86
Rohan Murmu <scythrine05@gmail.com>
2025-10-13 14:30:06 +0530

    🚫 fix: Reject Handler for avoid reject conflicts

--- src/services/user.request.service.js
+++ src/services/user.request.service.js
@@ -1817,7 +1817,7 @@ export const acceptRequestByManager = async (
         const data = {
             managerAcceptance: isAccept,
             managerAcceptanceId: managerId,
-            status: isAccept ? "MANAGER" : "REJECTED",
+            status: isAccept ? "APPROVED" : "REJECTED",
             remarkByManager: remark ?? null,
             overAllStatus,
             managerResponseTiming: new Date(),
