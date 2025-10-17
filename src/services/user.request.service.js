81a68563a5b279f5b3941ed230ff7d801f4bf40c
Sridharjeyakumar <sridhardk7@gmail.com>
2025-10-17 15:15:11 +0530

    working on admin modify separate request

--- src/services/user.request.service.js
+++ src/services/user.request.service.js
@@ -344,6 +344,7 @@ export const editRequest = async (
             optimizeTimeFrom,
             optimizeTimeTo,
             date,
+            optimizeStatus: true,
             // ...(mobileView && { isSanctioned: true }),
         },
     });
